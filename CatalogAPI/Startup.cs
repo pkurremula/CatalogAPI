90032f3f4d1bd1bed50b26ad1ee638d5972c8223
Prabhanjan <pkurremula@gmail.com>
2022-12-03 15:58:00 -0500

    initial commit new

--- CatalogAPI/Startup.cs
+++ CatalogAPI/Startup.cs
@@ -75,7 +75,10 @@ namespace CatalogAPI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CatalogAPI v1"));
             }
 
-            app.UseHttpsRedirection();
+            if (env.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
+            }
 
             app.UseRouting();
             
